d74f896ab5efa45b7e8d4c45c5666b9cbcd366a3
Margarita Obraztsova <marharyta.obraztsova@gmail.com>
2018-01-23 22:57:43 +0200

    Add initial implementation

---
+++ script.js
@@ -0,0 +1,22 @@
+const first = document.querySelector('#number1');
+const second = document.querySelector('#number2');
+
+const result = document.querySelector('.result');
+
+if (window.Worker) {
+    const myWorker = new Worker('worker.js'); 
+    first.onchange = function() {
+      myWorker.postMessage([first.value,second.value]); // Sending message as an array to the worker
+      console.log('Message posted to worker');
+    };
+
+    second.onchange = function() {
+      myWorker.postMessage([first.value,second.value]);
+      console.log('Message posted to worker');
+    };
+
+    myWorker.onmessage = function(e) {
+      result.textContent = e.data;
+      console.log('Message received from worker');
+    }; 
+}
